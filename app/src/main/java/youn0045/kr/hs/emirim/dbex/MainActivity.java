3abc54b1df94098552a6f684c28eee3a18a3c564
justhis01010101 <offentsive@naver.com>
2017-06-13 20:33:15 +0900

    #3 event handler

--- app/src/main/java/youn0045/kr/hs/emirim/dbex/MainActivity.java
+++ app/src/main/java/youn0045/kr/hs/emirim/dbex/MainActivity.java
@@ -5,13 +5,41 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editName, editCount, editResultName, editResultCount;
+    Button but_init;
+    Button but_insert;
+    Button but_select;
+    MyDBHelper myHelper;
+    SQLiteDatabase sqldb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editName = (EditText) findViewById(R.id.edit_group_name);
+        editCount = (EditText) findViewById(R.id.edit_group_cnt);
+        editResultName = (EditText) findViewById(R.id.edit_result_name);
+        editResultCount = (EditText) findViewById(R.id.edit_result_cnt);
+        but_init = (Button) findViewById(R.id.but_init);
+        but_insert = (Button) findViewById(R.id.but_insert);
+        but_select = (Button) findViewById(R.id.but_select);
+
+        // 데이터 베이스 생성
+        myHelper= new MyDBHelper(this);
+        //기존의 테이블이 존재하면 삭제하고 테이블을 새로 생성한다.
+        but_init.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view){
+                sqldb= myHelper.getWritableDatabase();
+                myHelper.onUpgrade(sqldb,1,2);
+                sqldb.close();
+            }
+        });
     }
     class MyDBHelper extends SQLiteOpenHelper{
         //생성자 호출 시 저스디스라는 데이터베이스가 생성된다
@@ -31,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
             String sql= "drop table if exist justhis table"; //만약에 저스디스 테이블이 존재한다면 테이블을 삭제해라
             sqLiteDatabase.execSQL(sql);
             onCreate(sqLiteDatabase);
+
         }
     }
 }
